e8738c828ac1d471bf350ad50a98c32b38725b94
TurnaFahmida <145374355+Turnarobbo@users.noreply.github.com>
2024-11-19 23:10:26 +0600

    Add files via upload

--- Lab-4/email.php
+++ Lab-4/email.php
@@ -2,13 +2,16 @@
 session_start();
 if (isset($_POST['submit'])) {
     $name = $_REQUEST['name'];
-    if (empty($name) || strpos($name, "@") == false || strpos($name, ".") == false)
-    {
-        header('location: Lab4_2.html');
+
+    // Check if the input is empty
+    if ($name == null) {
+        echo "Email can't be empty";
+    } else if (strpos($name, "@") === false) {
+        echo "Must contain '@' to be a valid email address";
+    } else if (strpos($name, ".", strpos($name, "@")) === false) {
+        echo "Must contain '.' after '@' to be a valid email address";
     } else {
-     
-        echo "Welcome Home";
+        echo "Email is saveed";
     }
 }
 ?>
- 
